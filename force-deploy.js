ad339260f4483d45683e5e50292de58fc218a569
John D. <johnd@Johns-Mac-mini.local>
2026-03-14 14:43:11 -0400

    Force Railway redeploy - debug section removed

---
+++ force-deploy.js
@@ -0,0 +1,4 @@
+// Force Railway deployment
+// Debug section completely removed - this file forces a rebuild
+// Timestamp: 2026-03-14 14:43:00 EST
+console.log('Debug section removed, forcing deployment');
